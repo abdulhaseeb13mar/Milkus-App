d9a36ca32548ae48cc2004eaf30416885c9d9102
Abdul Haseeb <abdulhaseeb13mar@gmail.com>
2020-06-15 11:14:52 +0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+import 'firebase/firestore';
+import firebaseConfig from './firebaseConfig.json';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
+
+export default firebase;
